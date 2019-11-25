68943613e3507c756c771dec494b9c400dc2a55e
Gabriela Boriero <gabrielamb2@al.inper.edu.br>
2019-11-25 16:14:31 -0300

    arrumando

--- assets/js/readonly.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	let test = document.querySelector('.test')
-
-	test.addEventListener('click', function() {
-		before()
-
-		setTimeout(after, 2000)
-	})
-})
